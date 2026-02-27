920236301ea006992c9dc035b64ee08929b85cf5
Kurt Madel <kmadel@mac.com>
2026-02-27 12:33:08 -0500

    updated pod-node image to 0.1.6

--- src/tenancy-models/auto-nodes/node/variables.tf
+++ src/tenancy-models/auto-nodes/node/variables.tf
@@ -5,7 +5,7 @@
 variable "image" {
   description = "Container image for the pod-node."
   type        = string
-  default     = "ghcr.io/loft-demos/pod-node:0.1.5"
+  default     = "ghcr.io/loft-demos/pod-node:0.1.6"
 }
 
 variable "image_pull_policy" {
